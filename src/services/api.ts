9b9c1a39202cd4ee630b6c98c9c83dbdb87904f1
Knufle <iamknufle@gmail.com>
2022-03-17 19:47:24 -0300

    feat: adding environment baseURL condition

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const environment = process.env.NODE_ENV || 'development';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: environment === 'production' ? 'https://knufle-sticky-notes-api.herokuapp.com' : 'http://localhost:3333'
 })
 
 export default api;
